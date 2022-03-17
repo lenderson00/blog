f9bdc43a6d52e6e491aaa61649e388999330f92c
Lenderson Macedo <github@lenderson.com.br>
2022-03-16 21:56:12 -0400

    chore: ensure return an list of Article if tag folder is not empty

--- tests/lib/article.spec.ts
+++ tests/lib/article.spec.ts
@@ -1,13 +1,65 @@
-const getAllArticles = (): string[] => {
-  return []
+import { mocked } from 'jest-mock'
+import { getAllTags } from '../../src/lib/tags'
+import fs from 'fs'
+import path from 'path'
+
+jest.mock('../../src/lib/tags')
+jest.mock('fs')
+
+type Articles = {
+  tag: string
+  articles: string[]
+}
+const getAllArticles = (): Articles[] => {
+  const tag = getAllTags()
+
+  const getArticleFromFolder = (value: string): Articles => {
+    const files = fs
+      .readdirSync(path.join(process.cwd(), `src/articles/${value}`))
+      .map(item => item.split('.')[0])
+
+    return {
+      tag: value,
+      articles: files
+    }
+  }
+
+  const articles = tag.map(getArticleFromFolder)
+
+  return articles
 }
 
 describe('Article Helpers', () => {
   describe('GetAllArticles', () => {
     it('Should return an empty array of Article if [tag] folder is empty', () => {
+      const getAllTagsStub = jest.fn().mockImplementationOnce(() => {
+        return []
+      })
+      mocked(getAllTags).mockImplementationOnce(getAllTagsStub)
       const sut = getAllArticles()
 
       expect(sut).toEqual([])
+    })
+
+    it('Should return an list of Article if tag folder is not empty', () => {
+      const getAllTagsStub = jest.fn().mockImplementationOnce(() => {
+        return ['css']
+      })
+      mocked(getAllTags).mockImplementationOnce(getAllTagsStub)
+
+      const readdirSyncStub = jest.fn().mockImplementationOnce(() => {
+        return ['hello-world']
+      })
+      mocked(fs.readdirSync).mockImplementationOnce(readdirSyncStub)
+
+      const sut = getAllArticles()
+
+      expect(sut).toEqual([{
+        tag: 'css',
+        articles: [
+          'hello-world'
+        ]
+      }])
     })
   })
 })
