a5ab181b8d1b9e82f8d1f7f756a67d65f9b95306
Lenderson Macedo <github@lenderson.com.br>
2022-03-20 18:05:43 -0400

    chore: add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <>
-      Lenderson Macedo - Home
-    </>
+    <h1 className="text-3xl font-bold underline">
+  Lenderson Macedo - Home
+    </h1>
   )
 }
 
